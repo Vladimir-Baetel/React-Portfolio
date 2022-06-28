8bb3a89b96900ce2ccd5b4f64eb84691da26e71e
Vladimir-Baetel <vladimir.baetel@gmail.com>
2022-06-28 10:47:07 +0300

    more layout improvements + skills bar loading animation

--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,11 @@
 import { Skills, Testimonials } from "../components";
 
 const About = () => {
-	return (
-		<section id="about">
-			<Skills />
-			<div className="max-w-7xl px-4 py-16 mx-auto flex flex-col gap-16">
-				{/* <pre className="mx-auto">
+  return (
+    <section id="about">
+      <Skills />
+      <div className="max-w-7xl px-4 py-16 mx-auto flex flex-col gap-16">
+        {/* <pre className="mx-auto">
 					<p className="md:text-6xl sm:text-4xl text-2xl">
 						<span className="text-violet-700">console</span>.
 						<span className="text-green-400">log</span>
@@ -77,80 +77,91 @@ const About = () => {
 						</a>
 					</div>
 				</div> */}
-				<div className="w-full flex gap-16">
-					<div className="w-full">
-						<div className="h-96 bg-white rounded-lg shadow-lg"></div>
-					</div>
-					<div className="w-full flex flex-col justify-center gap-6">
-						<div className="flex gap-4 items-center">
-							<div>
-								<p className="text-8xl font-bold text-violet-600">
-									2<span className="text-6xl">+</span>
-								</p>
-							</div>
-							<div className="flex-col text-3xl">
-								<p>years of experience</p>
-								<p>as a frontend developer</p>
-							</div>
-						</div>
-						<p className="text-2xl text-justify">
-							What started out as doing simple customizations for Shopify stores
-							quickly turned into a passion for building and designing clean,
-							modern, fully-responsive and pixel-perfect websites from scratch
-							using React, Next and Tailwind.
-						</p>
-						<p className="flex items-center text-xl italic">
-							Check out my
-							<a
-								className="mx-2 inline-flex items-center text-violet-600 hover:text-violet-700 border-b border-violet-600"
-								href=""
-							>
-								<i className="mr-1 fa-brands fa-linkedin"></i> LinkedIn
-							</a>{" "}
-							profile for more
-						</p>
-					</div>
-				</div>
-				<div className="w-full flex gap-16">
-					<div className="w-full flex flex-col justify-center gap-6">
-						<p className="text-3xl">Hobbies and interests of mine:</p>
-						<div className="flex flex-wrap gap-2 text-xl">
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-blue-400 to-blue-500 shadow-lg">
-								🖥️ computers
-							</p>
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-green-400 to-green-500 shadow-lg">
-								💵 personal finance
-							</p>
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-red-400 to-red-500 shadow-lg">
-								🚗 cars
-							</p>
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-slate-400 to-slate-500 shadow-lg">
-								👽 space
-							</p>
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-lg">
-								🍝 cooking
-							</p>
-							<p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-violet-400 to-violet-500 shadow-lg">
-								🕹️ gaming
-							</p>
-						</div>
+        <div className="w-full flex gap-16">
+          <div className="w-full">
+            <div className="h-96 bg-white rounded-lg shadow-lg"></div>
+          </div>
+          <div className="w-full flex flex-col justify-center gap-6">
+            <div className="flex gap-4 items-center">
+              <div>
+                <p className="text-8xl font-bold text-violet-600">
+                  2<span className="text-6xl">+</span>
+                </p>
+              </div>
+              <div className="flex-col text-3xl">
+                <p>years of experience</p>
+                <p>as a frontend developer</p>
+              </div>
+            </div>
+            <p className="text-2xl text-justify">
+              What started out as doing simple customizations for Shopify stores
+              quickly turned into a passion for building and designing clean,
+              modern, fully-responsive and pixel-perfect websites from scratch
+              using React, Next and Tailwind.
+            </p>
+            <p className="flex items-center text-xl">
+              Check out my
+              <a
+                className="mx-2 inline-flex items-center text-violet-600 hover:text-violet-700 border-b border-violet-600"
+                href=""
+              >
+                <i className="mr-1 fa-brands fa-linkedin"></i> LinkedIn
+              </a>{" "}
+              profile for more
+            </p>
+          </div>
+        </div>
+        <div className="w-full flex gap-16">
+          <div className="w-full flex flex-col justify-center gap-6">
+            <p className="text-3xl">Hobbies and interests of mine:</p>
+            <div className="flex flex-wrap gap-2 text-xl">
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-blue-400 to-blue-500 shadow-lg">
+                🖥️ computers
+              </p>
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-green-400 to-green-500 shadow-lg">
+                💵 personal finance
+              </p>
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-red-400 to-red-500 shadow-lg">
+                🚗 cars
+              </p>
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-slate-400 to-slate-500 shadow-lg">
+                👽 space
+              </p>
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-yellow-400 to-yellow-500 shadow-lg">
+                🍝 cooking
+              </p>
+              <p className="w-fit px-4 py-1 items-center rounded-full bg-gradient-to-b from-violet-400 to-violet-500 shadow-lg">
+                🕹️ gaming
+              </p>
+            </div>
 
-						<a
-							className="italic text-xl"
-							href="https://youtu.be/5PsnxDQvQpw?t=15"
-							target="blank"
-						>
-							<q>I'm just a regular, everyday, normal guy...</q>
-						</a>
-					</div>
-					<div className="w-full">
-						<div className="h-96 bg-white rounded-lg shadow-lg"></div>
-					</div>
-				</div>
-			</div>
-			<Testimonials />
-		</section>
-	);
+            <a
+              className="text-xl"
+              href="https://youtu.be/5PsnxDQvQpw?t=15"
+              target="blank"
+            >
+              <q>I'm just a regular, everyday, normal guy...</q>
+            </a>
+          </div>
+          <div className="w-full">
+            <div className="h-96">
+              {/* <iframe
+                className="rounded-lg shadow-lg"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/5PsnxDQvQpw?start=12"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Testimonials />
+    </section>
+  );
 };
 
 export default About;
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,63 +1,79 @@
+import { useEffect } from "react";
+
 const Skills = () => {
+  const skillsAnimation = () => {
+    useEffect(() => {
+      for (
+        let i = 0;
+        i < document.querySelector(".skill-cont").children.length;
+        i++
+      ) {
+        document.querySelector(".skill-cont").children[i].style.animationDelay =
+          150 * i + "ms";
+      }
+    });
+  };
+  skillsAnimation();
+
   return (
     <div className="flex bg-gradient-to-b from-violet-600 to-violet-700">
       <div className="w-full max-w-7xl mx-auto flex">
-        <div className="w-full flex gap-2 md:justify-around justify-start md:overflow-x-none overflow-x-scroll">
-          <div className="p-2 flex flex-col items-center">
+        <div className="skill-cont w-full flex gap-2 md:justify-around justify-start md:overflow-x-hidden overflow-x-scroll text-white">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="HTML_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">HTML</p>
+            <p className="-mt-2 mb-2">HTML</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img className="w-20 h-20 min-w-[5rem]" src="CSS_icon.png" alt="" />
-            <p className="-mt-2 mb-2 text-white">CSS</p>
+            <p className="-mt-2 mb-2">CSS</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img className="w-20 h-20 min-w-[5rem]" src="JS_icon.png" alt="" />
-            <p className="-mt-2 mb-2 text-white">JavaScript</p>
+            <p className="-mt-2 mb-2">JavaScript</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="REACT_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">React</p>
+            <p className="-mt-2 mb-2">React</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="TAILWIND_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">Tailwind</p>
+            <p className="-mt-2 mb-2">Tailwind</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="SASS_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">SASS</p>
+            <p className="-mt-2 mb-2">SASS</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="JQUERY_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">jQuery</p>
+            <p className="-mt-2 mb-2">jQuery</p>
           </div>
-          <div className="p-2 flex flex-col items-center">
+          <div className="fade-in p-2 flex flex-col items-center">
             <img
               className="w-20 h-20 min-w-[5rem]"
               src="BOOTSTRAP_icon.png"
               alt=""
             />
-            <p className="-mt-2 mb-2 text-white">Bootstrap</p>
+            <p className="-mt-2 mb-2">Bootstrap</p>
           </div>
         </div>
       </div>
